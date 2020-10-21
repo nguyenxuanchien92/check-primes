3d58fefddbca909a8501f3294198ab99021629d0
nguyenxuanchien92 <nguyenxuanchien92@gmail.com>
2020-10-21 15:52:52 +0700

    chỉnh sửa lại điều kiện của tham số truyền vào

--- check-primes.js
+++ check-primes.js
@@ -1,22 +1,25 @@
 function isPrimes(num) {
 
     let result;
+    let str = "";
+    if (num < 2) {
+        console.log("nhập lại giá trị")
+    } else if (num === 2) {
+        str += num;
+    } else {
+        for (num; num < 10000; num++) {
+            for (let i = 2; i < num; i++) {
 
-
-    let str="2,";
-    for (let num = 2; num < 10000; num++) {
-
-        for (let i = 2; i < num; i++) {
-
-            if (num % i === 0) {
-                result = false;
-                break;
-            } else {
-                result = true;
+                if (num % i === 0) {
+                    result = false;
+                    break;
+                } else {
+                    result = true;
+                }
             }
-        }
-        if (result) {
-            str += num+",";
+            if (result) {
+                str += num + ",";
+            }
         }
     }
 
